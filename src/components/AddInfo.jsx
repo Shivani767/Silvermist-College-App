596dcc00236d63fda8077d42dc6a31827803aa35
Shivani Bhandari <shivanibhandari767@gmail.com>
2024-09-28 23:15:38 +0530

    Add files via upload

---
+++ src/components/AddInfo.jsx
@@ -0,0 +1,274 @@
+import React, { useState } from "react";
+import "./css/form.css";
+import HeroBanner from "./HeroBanner";
+import axios from "axios";
+import OTP from "./OTP";
+
+export default function AddInfo() {
+  const [inputs, setInputs] = useState({});
+  const [image, setImage] = useState("");
+  const [link1, setLink1] = useState("");
+  const [link2, setLink2] = useState("");
+  const [link3, setLink3] = useState("");
+  const [disabled, setdisabled] = useState(false);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setdisabled(true);
+
+    const links = [link1, link2, link3];
+    const data = { ...inputs, links, image };
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("name", data.name);
+    formData.append("adress", data.address);
+    formData.append("city", data.city);
+    formData.append("email", data.email);
+    formData.append("OTP", data.OTP);
+    formData.append("roll_no", data.roll_no);
+    formData.append("phone_no", data.phone_no);
+    formData.append("about", data.about);
+    formData.append("branch", data.branch);
+    formData.append("links", data.links);
+
+    await axios
+      .post(`https://dic-backend.onrender.com/student/upload`, formData)
+      .then((res) => {
+        alert(res.data.Message);
+        setImage("");
+        setLink1("");
+        setLink2("");
+        setLink3("");
+        setInputs({});
+      })
+      .catch((err) => {
+        alert("Error");
+      });
+    setdisabled(false);
+  };
+  return (
+    <>
+      <HeroBanner />
+      <OTP />
+      <div className="form-container">
+        <form
+          className="register-form form-style-10"
+          onSubmit={handleSubmit}
+          method="post"
+          enctype="multipart/form-data"
+        >
+          <fieldset disabled={disabled}>
+            <h1>Register</h1>
+            <div className="section">
+              <span>1</span>Personal Info
+            </div>
+            <div className="inner-wrap">
+              <label>
+                Add profile Image:
+                <img
+                  style={{
+                    height: "100px",
+                    width: "100px",
+                    borderRadius: "100%",
+                    marginLeft: "30px",
+                    marginBottom: "20px",
+                  }}
+                  src={image === "" ? "" : URL.createObjectURL(image)}
+                  alt="preview"
+                />
+                <input
+                  type="file"
+                  name="avatar"
+                  accept=".jpg"
+                  onChange={(event) => {
+                    setImage(event.target.files[0]);
+                  }}
+                />
+              </label>
+              <label>
+                Name *
+                <input
+                  type="text"
+                  name="name"
+                  required="true"
+                  className="form-field"
+                  placeholder="Abhishek Goyal"
+                  value={inputs.name || ""}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Address
+                <input
+                  type="text"
+                  name="address"
+                  className="form-field"
+                  placeholder="Nai abadi street no 6"
+                  value={inputs.address || ""}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                City
+                <input
+                  type="text"
+                  name="city"
+                  className="form-field"
+                  placeholder="Abohar, Punjab, India"
+                  value={inputs.city || ""}
+                  onChange={handleChange}
+                />
+              </label>
+
+              <label>
+                Email *
+                <input
+                  type="email"
+                  name="email"
+                  className="form-field"
+                  required="true"
+                  placeholder="xyz@hmail.com"
+                  value={inputs.email || ""}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Phone Number
+                <input
+                  type="text"
+                  name="phone_no"
+                  className="form-field"
+                  placeholder="905*****55"
+                  value={inputs.phone_no || ""}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                About Yourself
+                <input
+                  type="text"
+                  style={{ height: "80px" }}
+                  name="about"
+                  className="form-field"
+                  placeholder="Hello my name is ...."
+                  value={inputs.about || ""}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+
+            <div className="section">
+              <span>2</span>University Info
+            </div>
+            <div className="inner-wrap">
+              <label>
+                Roll No *
+                <input
+                  type="text"
+                  name="roll_no"
+                  className="form-field"
+                  placeholder="UE208007"
+                  required="true"
+                  value={inputs.roll_no || ""}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Branch *
+                <input
+                  type="text"
+                  name="branch"
+                  className="form-field"
+                  required="true"
+                  placeholder="Information Technology"
+                  value={inputs.branch || ""}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="section">
+              <span>3</span>Personal/Work Links
+            </div>
+            <div className="inner-wrap">
+              <label>
+                Links 1
+                <input
+                  type="text"
+                  name="link1"
+                  required="true"
+                  className="form-field"
+                  value={link1 || ""}
+                  onChange={(event) => {
+                    setLink1(event.target.value);
+                  }}
+                />
+              </label>
+              <label>
+                Links 2
+                <input
+                  type="text"
+                  name="link1"
+                  required="true"
+                  className="form-field"
+                  value={link2 || ""}
+                  onChange={(event) => {
+                    setLink2(event.target.value);
+                  }}
+                />
+              </label>
+              <label>
+                Links 3
+                <input
+                  type="text"
+                  name="link1"
+                  required="true"
+                  className="form-field"
+                  value={link3 || ""}
+                  onChange={(event) => {
+                    setLink3(event.target.value);
+                  }}
+                />
+              </label>
+            </div>
+            <div className="section">
+              <span>4</span>OTP
+            </div>
+            <div className="inner-wrap">
+              <label>
+                OPT*
+                <input
+                  type="text"
+                  name="OTP"
+                  className="form-field"
+                  placeholder="123456"
+                  value={inputs.OTP || ""}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="button-section">
+              <button
+                className="waves-effect waves-light btn center-align white-text text-darken-2 card-panel indigo darken-4  z-depth-1"
+                style={{ backgroundColor: "#036089", color: "white" }}
+                onClick={(event) => {
+                  handleSubmit(event);
+                }}
+              >
+                Register
+              </button>
+              <span className="privacy-policy">
+                You agree to our Terms and Policy.
+              </span>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./css/contact us.css";
+import axios from "axios";
+
+export default function ContactUs() {
+  // Form Validation
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    let firstname = event.target[0].value;
+    let lastname = event.target[1].value;
+    let email = event.target[2].value;
+    let querry = event.target[3].value;
+
+    await axios
+      .post("https://dic-backend.onrender.com/querry", {
+        firstname,
+        lastname,
+        email,
+        querry,
+      })
+      .then((res) => {
+        console.log(res);
+        alert("Success", res);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error"+err);
+      });
+      document.getElementById("querry-form").reset();
+  }
+
+  return (
+    <>
+      <div style={{ textAlign: "center" }} data-aos="fade-up">
+        <img
+          src="https://img.freepik.com/free-vector/organic-flat-customer-support-illustration_23-2148899174.jpg?w=900&t=st=1688244260~exp=1688244860~hmac=85062763b103ceedf79de42fcda51e79d87842cdc10414745082e8d013a54939"
+          width={"80%"}
+          alt="stu"
+        />
+      </div>
+      <form id="querry-form" onSubmit={handleSubmit}>
+        <h1>Querry/Contact Form</h1>
+        <hr />
+        <h3>User Details:</h3>
+        <label for="firstname">First name: </label>
+        <input type="text" name="firstname" placeholder="Abhishek" required />
+        &nbsp;&nbsp;
+        <label for="lastname">Last name: </label>
+        <input type="text" name="lastname" placeholder="Goyal" required />
+        <br />
+        <br />
+        <label for="Email">Email(optional): </label>
+        <input type="email" name="Email" placeholder="xyz@gmail.com" />
+        <br />
+        <br />
+        <h3>Querry:</h3>
+        <textarea
+          rows="6"
+          name="querry"
+          placeholder="Describe your querry here..."
+        ></textarea>
+        <br />
+        <br />
+        <input type="reset" value="Reset" class="button" />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="submit" value="Send" class="button" />
+        <br />
+      </form>
+
+      <div class="con">
+        <h1>Contact Us</h1>
+        <hr />
+        <ul>
+          <li>University Institute of Engineering and Technology</li>
+          <li>Sector 25, South Campus,</li>
+          <li>Panjab University,</li>
+          <li>Chandigarh (Union Territory), INDIA</li>
+          <li>PINCODE: 160014</li>
+          <li>0172-2541242</li>
+          <li>directoruiet@pu.ac.in</li>
+          <br />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3511237.761840447!2d72.1428915875!3d30.74809900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390feda45e945a31%3A0xdc967b3a7e0f1e95!2sUniversity%20Institute%20Of%20Engineering%20and%20Technology%2C%20Panjab%20University!5e0!3m2!1sen!2sin!4v1686501691180!5m2!1sen!2sin"
+            width="200vh"
+            height="200vh"
+            style={{ border: "0" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </ul>
+      </div>
+    </>
+  );
+}
